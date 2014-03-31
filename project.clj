7765e0f092730a76cd3089bf3528d04174da404f
Will Sommers <will.sommers@gmail.com>
2014-03-31 13:57:04 -0400

    More Setup

--- project.clj
+++ project.clj
@@ -2,14 +2,20 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2138"]]
+  :dependencies [[ankha "0.1.1"]
+								 [org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2138"]
+								 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [om "0.5.1"]
+                 [prismatic/dommy "0.1.2"]
+                 [sablono "0.2.6"]
+                 [secretary "1.0.0"]]
 
   :plugins [[lein-cljsbuild "1.0.2"]]
 
   :source-paths ["src"]
 
-  :cljsbuild { 
+  :cljsbuild {
     :builds [{:id "script-ed-annotate"
               :source-paths ["src"]
               :compiler {
